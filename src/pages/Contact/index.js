475cf7979ccd89e94899f65d6fffc48d3ee753a8
Paulo Farah <paulofarahalves@hotmail.com>
2023-05-30 14:05:24 -0300

    editing contact submit action

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -4,10 +4,15 @@ import { motion } from 'framer-motion';
 import Header from '../../components/partials/Header';
 import Footer from '../../components/partials/Footer';
 import { PageArea } from './styled';
+import { useState } from 'react';
 
 export const ContactUs = () => {};
 
 export default function Contact() {
+	const [nameInput, setNameInput] = useState('');
+	const [emailInput, setEmailInput] = useState('');
+	const [messageInput, setMessageInput] = useState('');
+
 	const form = useRef();
 
 	const sendEmail = (e) => {
@@ -23,7 +28,12 @@ export default function Contact() {
 			.then(
 				(result) => {
 					console.log(result.text);
+					alert('Message sent!');
+					setNameInput('');
+					setEmailInput('');
+					setMessageInput('');
 				},
+
 				(error) => {
 					console.log(error.text);
 				}
@@ -41,17 +51,29 @@ export default function Contact() {
 							type="text"
 							name="user_name"
 							placeholder="Your name"
+							value={nameInput}
+							onChange={(e) => {
+								setNameInput(e.target.value);
+							}}
 						/>
 
 						<input
 							type="email"
 							name="user_email"
 							placeholder="Your email"
+							value={emailInput}
+							onChange={(e) => {
+								setEmailInput(e.target.value);
+							}}
 						/>
 
 						<textarea
 							name="message"
 							placeholder="Your message..."
+							value={messageInput}
+							onChange={(e) => {
+								setMessageInput(e.target.value);
+							}}
 						/>
 						<input type="submit" value="Send" />
 					</form>
