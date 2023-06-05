5ba00925e57d2986de8bd41e3f59c55dfd952a38
Paulo Farah <paulofarahalves@hotmail.com>
2023-06-05 13:44:58 -0300

    editing routes

--- src/components/partials/Header/index.js
+++ src/components/partials/Header/index.js
@@ -7,22 +7,37 @@ export default function Header() {
 		<HeaderArea>
 			<div className="container">
 				<div className="leftSide">
-					<Link to={'/home'}>JOHN DOE</Link>
+					<Link to={'/portfolio-react/home'}>JOHN DOE</Link>
 				</div>
 				<div className="rightSide">
-					<NavLink to={'/home'} activeClassName="active">
+					<NavLink
+						to={'/portfolio-react/home'}
+						activeClassName="active"
+					>
 						HOME
 					</NavLink>
-					<NavLink to={'/work'} activeClassName="active">
+					<NavLink
+						to={'/portfolio-react/work'}
+						activeClassName="active"
+					>
 						WORK
 					</NavLink>
-					<NavLink to={'/about'} activeClassName="active">
+					<NavLink
+						to={'/portfolio-react/about'}
+						activeClassName="active"
+					>
 						ABOUT
 					</NavLink>
-					<NavLink to={'/story'} activeClassName="active">
+					<NavLink
+						to={'/portfolio-react/story'}
+						activeClassName="active"
+					>
 						STORY
 					</NavLink>
-					<NavLink to={'/contact'} activeClassName="active">
+					<NavLink
+						to={'/portfolio-react/contact'}
+						activeClassName="active"
+					>
 						CONTACT
 					</NavLink>
 				</div>
